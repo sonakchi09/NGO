6ea924d77c96d5849e9cc8ece1f635f35590d36a
Ishayan kundu <ishayankundu6@gmail.com>
2026-02-22 15:11:15 +0530

    updated

--- NGO-WEBSITE-WITH-NEXT-JS-/src/app/donate/page.tsx
+++ NGO-WEBSITE-WITH-NEXT-JS-/src/app/donate/page.tsx
@@ -30,7 +30,7 @@ const handlePayment = async () => {
     };
 
     const response = await axios.post(
-  "https://ngo-backend-plum.vercel.app/api/donation",
+  "https://ngobackend-ashen.vercel.app/api/donation",
   payload
 );
 
--- NGO-WEBSITE-WITH-NEXT-JS-/src/app/volunteer/VolunteerFormModal.tsx
+++ NGO-WEBSITE-WITH-NEXT-JS-/src/app/volunteer/VolunteerFormModal.tsx
@@ -39,7 +39,7 @@ export default function VolunteerFormModal({
 
     try {
       const res = await axios.post(
-  "http://localhost:5000/api/volunteer",
+  "https://ngobackend-ashen.vercel.app/api/volunteer",
   formData
 );
 
