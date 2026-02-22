27e777ca057f2c57e73deaa332b30113a6931291
Ishayan kundu <ishayankundu6@gmail.com>
2026-02-22 14:49:17 +0530

    changed

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/ngoBackend`);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected");
   } catch (err) {
     console.error("DB connection error:", err.message);
